f5a3fef6ca4451b044650efae27ca100e5a8e83a
AliH78 <56169959+AliH-78@users.noreply.github.com>
2023-03-12 14:30:44 +0300

    Update console.py

--- utils/console.py
+++ utils/console.py
@@ -1,5 +1,5 @@
 import datetime
 import colorama
 
-def print_log(module, message, module_color = colorama.Fore.LIGHTBLACK_EX, message_color = colorama.Fore.YELLOW, with_date = True, date_color = colorama.Fore.WHITE):
+def print_module_message(module, message, module_color = colorama.Fore.LIGHTBLACK_EX, message_color = colorama.Fore.YELLOW, with_date = True, date_color = colorama.Fore.WHITE):
     return print(f"{date_color}{datetime.datetime.strftime(datetime.datetime.now(), '[%d/%m/%Y %H:%M:%S]') if with_date else ''} {module_color}[{module}] {message_color}{message}")
