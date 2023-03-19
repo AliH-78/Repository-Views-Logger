5f1f3450eb002bcf1979eb8fa2571a9c59f2f39a
AliH78 <56169959+AliH-78@users.noreply.github.com>
2023-03-19 18:17:36 +0300

    Update log.py

--- utils/log.py
+++ utils/log.py
@@ -5,7 +5,7 @@ class Log:
     def __init__(self, filename, file_mode = "a"):
         os.makedirs(os.path.dirname(os.path.abspath(filename)), exist_ok = True)
 
-        self.handle = open(filename, file_mode)
+        self.handle = open(os.path.abspath(filename), file_mode)
     
     def write_error(self, message):
         self.handle.write(f"{datetime.datetime.strftime(datetime.datetime.now(), '[%d/%m/%Y %H:%M:%S]')} ERROR [!] {message}")
